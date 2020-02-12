7d00d9380eb0f5bf9eaf5fbfd1653871d28af356
Joe Fuller-Gray <joefg@protonmail.com>
2020-02-12 21:16:40 +0000

    Serving raw geojson through /api endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
+import json
+
 from flask import render_template
 
 from app import app
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     return render_template('index.html', title='jfg-gis')
+
+@app.route('/api')
+def serve_map():
+    features = {}
+    with open('data/wifi.geojson', 'r') as f:
+        features = json.load(f)
+    return features
