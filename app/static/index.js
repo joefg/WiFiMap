f6de1bf095ebd37f495a8bcbd6c7922e6d462cf9
Joe Fuller-Gray <joefg@protonmail.com>
2020-04-18 14:46:29 +0100

    Added pretty encapsulation for map. Might make it modular later?

--- app/static/index.js
+++ app/static/index.js
@@ -1,18 +1,24 @@
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
-        }),
-        new ol.layer.Vector({
-            source: new ol.source.Vector({
-                url: '/api',
-                format: new ol.format.GeoJSON()
+function gis(){
+    return {
+        'map' : new ol.Map({
+            target: 'map',
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                }),
+                new ol.layer.Vector({
+                    source: new ol.source.Vector({
+                        url: '/api',
+                        format: new ol.format.GeoJSON()
+                    })
+                })
+            ],
+            view: new ol.View({
+                center: ol.proj.fromLonLat([37.41, 8.82]),
+                zoom: 4
             })
         })
-    ],
-    view: new ol.View({
-        center: ol.proj.fromLonLat([37.41, 8.82]),
-        zoom: 4
-    })
-});
+    }
+}
+
+var gis = gis()
