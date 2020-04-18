a176d83af2e2c50e6743a400deb242e21b93b09b
Joe Fuller-Gray <joefg@protonmail.com>
2020-04-18 19:19:58 +0100

    Rejigged to use the Closure design pattern, got styles working!

--- app/static/index.js
+++ app/static/index.js
@@ -1,24 +1,38 @@
-function gis(){
-    return {
-        'map' : new ol.Map({
-            target: 'map',
-            layers: [
-                new ol.layer.Tile({
-                    source: new ol.source.OSM()
-                }),
-                new ol.layer.Vector({
-                    source: new ol.source.Vector({
-                        url: '/api',
-                        format: new ol.format.GeoJSON()
-                    })
+var gis = (function() {
+    var styles = {
+        'Point' : new ol.style.Style({
+            image: new ol.style.Circle({
+                radius: 5,
+                fill: null,
+                stroke: new ol.style.Stroke({
+                    color: 'red',
+                    width: 1
                 })
-            ],
-            view: new ol.View({
-                center: ol.proj.fromLonLat([37.41, 8.82]),
-                zoom: 4
             })
         })
     }
-}
 
-var gis = gis()
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            }),
+            new ol.layer.Vector({
+                source: new ol.source.Vector({
+                    url: '/api',
+                    format: new ol.format.GeoJSON()
+                }),
+                style: styles['Point']
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([37.41, 8.82]),
+            zoom: 4
+        })
+    })
+
+    return {
+        map : map
+    }
+})();
