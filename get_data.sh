fa819ca4d67529a54205d5863f1bc5c0cd25b48c
Joe Fuller-Gray <joefg@protonmail.com>
2020-06-01 21:34:04 +0100

    Update fetching script to convert our GeoJSON and TopoJSON data to spatialite

--- get_data.sh
+++ get_data.sh
@@ -1,2 +1,7 @@
+echo "Fetching data..."
 wget https://data.cambridgeshireinsight.org.uk/sites/default/files/Site%20list.geojson -O data/wifi.geojson
 wget https://martinjc.github.io/UK-GeoJSON/json/eng/topo_lad.json -O data/districts.topojson
+
+echo "Converting data..."
+ogr2ogr -f sqlite -dsco SPATIALITE=YES data/wifi.db data/wifi.geojson
+ogr2ogr -f sqlite -dsco SPATIALITE=YES data/districts.db data/districts.topojson
